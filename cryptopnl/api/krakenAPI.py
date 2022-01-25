64c9bca813e505e919cfbd641b195761cf662da5
ruizdesotto <miguel.ruizdesotto@gmail.com>
2022-01-25 21:47:40 +0100

    adding tests to krakenAPI

--- cryptopnl/api/krakenAPI.py
+++ cryptopnl/api/krakenAPI.py
@@ -36,7 +36,7 @@ class krakenAPI(exchangeAPI):
             print("No results retrieved")
             print(api_reply)
             raise
-        except APIException as e:
+        except APIException as e: # Maybe not here or like here
             print(f"API call failed {e}")
             raise 
 
@@ -48,30 +48,37 @@ class krakenAPI(exchangeAPI):
         """
         method = "Time"
         response = krakenAPI._publicAPI(method)
-        unix_time = response[krakenAPI.UNIXTIME]
-        return dt.fromtimestamp(unix_time) 
+        try:
+            unix_time = response[krakenAPI.UNIXTIME]
+            return dt.fromtimestamp(unix_time) 
+        except KeyError as e:
+            raise APIException(e)
+        except OSError as e:
+            raise APIException(e)
+        
 
-    def getHistoryTrades(pair):
+    def getHistoryTrades(pair, since = None):
         """
         Get history trades for a particular cryptocurrency.
+        If since is not given, the last 1000 values will be given, 
+        else the 1000 following the timestamp are.
 
-        :returns: ([[price, vol, time, ...],], last)
+        :param pair: cryptocurrency pair
+        :param since: (optional) timestamp in ns to which start the query 
+        :returns: ([[price, vol, time (s), ...],], last (s))
         """
-        #TODO set interval of time
         method = "Trades"
-        response = krakenAPI._publicAPI(method, params = {"pair": pair})
+        params = {"pair": pair}
+        if since is not None: params["since"] = str(since)
+        response = krakenAPI._publicAPI(method, params)
         try:
             for v in response.values():
                 if isinstance(v, list): 
-                    # do the 3 thing
                     clean_data = [i[:3] for i in v]
-                    return (clean_data, response["last"])
-            raise KeyError("Pair not found")
-        except Exception as e:
-            print(f"Trades not retrieved: {e}")
-            return None
-
-        pass
+                    return (clean_data, float(response["last"])/1e9)
+        except KeyError as e:
+            raise APIException()
+        
 
     def getTickerAPI():
         """Get updated ticker information."""
--- cryptopnl/test/api/test_krakenAPI.py
+++ cryptopnl/test/api/test_krakenAPI.py
@@ -1,12 +1,11 @@
 from datetime import datetime as dt
 import pytest
 
+from cryptopnl.api.exchangeAPI import APIException
 from cryptopnl.api.krakenAPI import krakenAPI
 
-# Not sure if I should use mock values to test the class
-# Putting the API tag to methods that require connexion
-# Equivalent mock values for off-line testing
 # TODO test _publicAPI
+# Check in case of error
 
 MAX_WAIT_IN_SEC_ALLOWED = 3
 
@@ -14,71 +13,80 @@ def test_api_domain():
     """Verify correct end point."""
     assert krakenAPI.API_DOMAIN == "https://api.kraken.com" 
 
-@pytest.mark.apitest
-def test_getServerTime():
-    """Retrieves a date close to now"""
-    krakenTime = krakenAPI.getServerTime()
-    deltaSeconds = (dt.now()-krakenTime).total_seconds()
-    assert isinstance(krakenTime, dt)  
-    assert abs(deltaSeconds) < MAX_WAIT_IN_SEC_ALLOWED    
-    
-@pytest.mark.apitest
-def test_getHistoryTrades():
-    """
-    Retrieves information for a certain pair, ex. btceur
-    Assert the retieved json object has the correct keys and prices can be cast to numbers
-    """
-    pair = "XXBTZEUR"
-    data, timestamp = krakenAPI.getHistoryTrades(pair)
-    assert isinstance(data, list) 
-    items = data[0]
-    assert isinstance(items, list)
-    assert len(items) == 3
-    try:
-        float(items[0])
-        float(items[1])
-    except ValueError:
-        assert "string" == "number"
-    else:
-        assert dt.fromtimestamp(items[2])
-    assert isinstance(timestamp, str)
-
 def test_mock_getServerTime(mocker):
     """
     Assert the correct time is obtained
-    Mocking the api request (static method _publicAPI)
     """
     expected_result = {krakenAPI.UNIXTIME:1642967384}
-    mocker.patch("cryptopnl.api.krakenAPI.krakenAPI._publicAPI", return_value=expected_result) 
+    public_mock = mocker.patch("cryptopnl.api.krakenAPI.krakenAPI._publicAPI", return_value=expected_result) 
     krakenTime = krakenAPI.getServerTime()
+
+    public_mock.assert_called_once_with("Time")
     assert isinstance(krakenTime, dt)  
     assert krakenTime.timestamp() == expected_result[krakenAPI.UNIXTIME] 
+
+@pytest.mark.parametrize("output", 
+            ["Im drunk", {}, {krakenAPI.UNIXTIME:-1}])
+def test_mock_getServerTime_ImproperFormat(output, mocker):
+    """
+    Assert APIException if the time is not found 
+    """
+    mocker.patch("cryptopnl.api.krakenAPI.krakenAPI._publicAPI", return_value=output) 
+    with pytest.raises(APIException):
+        _ = krakenAPI.getServerTime()
 
 def test_mock_getHistoryTrades(mocker):
     """
     Assert the retrieved json object has the correct keys and prices can be cast to numbers
-    Mocking the api request (static method _publicAPI)
     """
     pair = "XXBTZEUR"
     expected_result = {pair:[["30668.70000","0.00455250",1642967070.7617,"b","l",""]],"last":"1642969452668122012"}
-    mocker.patch("cryptopnl.api.krakenAPI.krakenAPI._publicAPI", return_value=expected_result)
+    public_mock = mocker.patch("cryptopnl.api.krakenAPI.krakenAPI._publicAPI", return_value=expected_result)
     data, timestamp = krakenAPI.getHistoryTrades(pair)
-    assert isinstance(data, list) 
     items = data[0]
+
+    public_mock.assert_called_once_with("Trades", {"pair": pair})
+    assert isinstance(data, list) 
     assert isinstance(items, list)
     assert len(items) == 3
     assert float(items[0]) == float("30668.70000")
     assert float(items[1]) == float("0.00455250")
     assert isinstance(dt.fromtimestamp(items[2]), dt)
     assert dt.fromtimestamp(items[2]) == dt.fromtimestamp(1642967070.7617)
-    assert isinstance(timestamp, str)
+    assert isinstance(timestamp, float)
 
-def test_getTickerAPI():
+def test_mock_getHistoryTrades_sinceParam(mocker):
+    """
+    Assert the retrieved data is after a specific defined time 
+    """
+    pair = "XXBTZEUR"
+    since_time = 1643135792
+    expected_result = {pair:[["30668.70000","0.00455250",1643135792.000001,"b","l",""]],"last":"1643135792000001000"}
+    public_mock = mocker.patch("cryptopnl.api.krakenAPI.krakenAPI._publicAPI", return_value=expected_result)
+    data, timestamp = krakenAPI.getHistoryTrades(pair, since=since_time)
+    
+    public_mock.assert_called_once_with("Trades", {"pair": pair, "since": str(since_time)})
+    assert dt.fromtimestamp(data[0][2]) > dt.fromtimestamp(since_time)
+    assert dt.fromtimestamp(timestamp) > dt.fromtimestamp(since_time)
+
+@pytest.mark.parametrize("output", 
+            ["Im drunk", 
+             {"XXBTZEUR":[]}, 
+             {"XXBTZEUR":[["",]]},
+             {"XXBTZEUR":[["","",""]]},
+             {"XXBTZEUR":[["","",""]],"last":""}
+             ])
+def test_mock_getHistoryTrades_ImproperFormat(output, mocker):
+    """
+    Assert improper formats are dealt with 
+    """
+    pair = "XXBTZEUR"
+    mocker.patch("cryptopnl.api.krakenAPI.krakenAPI._publicAPI", return_value=output)
+
+    with pytest.raises(APIException):
+        _, _ = krakenAPI.getHistoryTrades(pair)
+
+def test_mock_getTickerAPI():
     """NOT IMPLEMENTED"""
     with pytest.raises(NotImplementedError):
         krakenAPI.getTickerAPI()
-
-def test_mock_getHistoryTrades_sinceParam():
-    assert False
-
-    
